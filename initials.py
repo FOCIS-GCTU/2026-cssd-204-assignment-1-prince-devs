624f862ac6364731a97b8669f839d594fb1d7441
Prince Larbi Addo <2425400511@live.gctu.edu.gh>
2026-05-31 05:02:00 +0000

    Completed Assignment 2

--- initials.py
+++ initials.py
@@ -1,7 +1,41 @@
 
-def main():
-  pass #code goes here
 
+# File: initials.py
+# Description: Print out my initials (P.L.A) from the name Prince Larbi Addo in  large letters.
+# Assignment Number: 2
+#
+# Name: Prince Larbi Addo
+# STUDENT ID: 2425400511
+# Email: 2425400511@live.gctu.edu.gh
+# Grader: Carolyn OR Emma OR Ahmad
+#
+# On my honor, Prince Larbi Addo, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+
+
+    # Print initials PLA in small (one line) and large (10-line block)
+    # block letter format. Each letter is 12 characters wide by 10 characters
+    # Three-dot separators appear before and after each letter and period block.
+    # A 2x2 asterisk period follows each letter on the final two rows.
+
+
+    print()
+    print("...PLA")
+    print()
+    print("...PPPPPPPPPP..........LLL.....................AAA..........")
+    print("...PPPPPPPPPPPP........LLL....................AA.AA.........")
+    print("...PPP.......PP........LLL...................AAA..AAA.......")
+    print("...PPPPPPPPPPPP........LLL..................AAA....AAA......")
+    print("...PPPPPPPPPP..........LLL.................AAAAAAAAAAAA.....")
+    print("...PPP.................LLL.................AAAAAAAAAAAA.....")
+    print("...PPP.................LLL.................AAA......AAA.....")
+    print("...PPP.................LLL.................AAA......AAA.....")
+    print("...PPP............**...LLLLLLLLLLLL...**...AAA......AAA...**")
+    print("...PPP............**...LLLLLLLLLLLL...**...AAA......AAA...**")
+    print()
 
 
 main()
